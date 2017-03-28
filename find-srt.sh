d87cc5caf8556e38d5896f0e77df577c1a202bff
clay726 <clay726@gmail.com>
2017-03-28 10:40:12 -0700

    Added comments to explain

--- find-srt.sh
+++ find-srt.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash #tells the os which program to use to interpert this script
 
 ls -lR $1 > raw.temp
 
-awk '
+awk ' 
 
 BEGIN { RS = "" } ;
 
@@ -11,23 +11,23 @@ BEGIN { RS = "" } ;
   if (x[2] !~ /\nd|\.srt\n/) {
     print x[1];
   }
-}
+} 
 
-' raw.temp > folders.temp
+' raw.temp > folders.temp #Thing long command sequence goes though and looks for all movies to find what you want awk is a language that is used to look though large amounts of data at once
 
-rm raw.temp
+rm raw.temp #Removes the temporary folder
 
-CURL_VERSION=$
+CURL_VERSION=$ #Defines the curlversion used 
 
-while read FOLDER; do
+while read FOLDER; do #When the code is being read do the following 
   VID_EXT_RE="\.(mpg|MPG|flv|FLV|wmv|WMV|avi|AVI|mkv|MKV|mov|MOV|mp4|MP4)$"
   for FILE in "$FOLDER"/*.*; do
     if [[ "$FILE" =~ $VID_EXT_RE ]]; then
-      MD5=$(./md5ex.sh "$FILE")
+      MD5=$(./md5ex.sh "$FILE") #Uses the code written in the other script and defenes MD5 as the list MD5 file that is created from the other program
       echo $FILE " " $MD5
-      #curl -A "SubDB/1.0 (curl/7.35.0 https://curl.haxx.se)" "http://api.thesubdb.com/?action=download&hash=$MD5&language=en" -o "$FOLDER/engsub.srt"
-    fi
+      #curl -A "SubDB/1.0 (curl/7.35.0 https://curl.haxx.se)" "http://api.thesubdb.com/?action=download&hash=$MD5&language=en" -o "$FOLDER/engsub.srt" This is the link to the website with   the subtitles 
+    fi 
   done
-done < folders.temp
+done < folders.temp #Tells you if it is ok to remove the folders.temp file:
 
-rm folders.temp
+rm folders.temp #Removes the folder used to holt the moves without subtitles 
--- md5ex.sh
+++ md5ex.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python #tells os which program to use to run the code
 
-import sys, os, hashlib
+import sys, os, hashlib #Brings in functions to be used by the code
 
-def get_hash(name):
-    readsize = 64 * 1024
-    with open(name, 'rb') as f:
-        size = os.path.getsize(name)
-        data = f.read(readsize)
-        f.seek(-readsize, os.SEEK_END)
-        data += f.read(readsize)
-    return hashlib.md5(data).hexdigest()
+def get_hash(name): #Defines the function get_hash which takes in the value of the name variable 
+    readsize = 64 * 1024 #Sets var read size to 64KB
+    with open(name, 'rb') as f: #Open file name as a binary stream called "F"
+        size = os.path.getsize(name) #In reality this does nothing becuase it defines a variable that is not used
+        data = f.read(readsize) #Reads the first 64KB of a file and stores in the binary stream
+        f.seek(-readsize, os.SEEK_END) #Jumps to the last 64KB of a file 
+        data += f.read(readsize) #Adds the last 64KB of data to the file 
+    return hashlib.md5(data).hexdigest() #Creates an mds mash of data in hexadecimal
     
-print get_hash(sys.argv[1]);
+print get_hash(sys.argv[1]); #Prints the end result of the cod
